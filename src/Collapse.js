cf09d50b3e42108253d6c0a8601321d3a48cbb78
biangang.bg <biangang.bg@alibaba-inc.com>
2015-12-10 16:12:28 +0800

    remove rc-collapse dep

--- src/Collapse.js
+++ src/Collapse.js
@@ -1,31 +1,127 @@
-/**
- * Collapse Component for uxcore
- * @author vicent.bian
- *
- * Copyright 2014-2015, Uxcore Team, Alinw.
- * All rights reserved.
- */
-import React from 'react';
-import RcCollapse from 'rc-collapse';
-const prefixCls = 'kuma-collapse';
+import React, { PropTypes, createClass, Children } from 'react';
+import CollapsePanel from './Panel';
 
-class Collapse extends React.Component {
+const Collapse = createClass({
+  statics: {
+    Panel: CollapsePanel,
+  },
 
-    constructor(props) {
-        super(props);
+  propTypes: {
+    prefixCls: PropTypes.string,
+    activeKey: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.arrayOf(PropTypes.string),
+    ]),
+    defaultActiveKey: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.arrayOf(PropTypes.string),
+    ]),
+    onChange: PropTypes.func,
+    accordion: PropTypes.bool,
+  },
+
+  getDefaultProps() {
+    return {
+      prefixCls: 'kuma-collapse',
+      onChange: () => {
+      },
+      accordion: false,
+    };
+  },
+
+  getInitialState() {
+    const { activeKey, accordion } = this.props;
+    let { defaultActiveKey } = this.props;
+    // If is not accordion mode, then, defaultActiveKey should be an array
+    if (!accordion) {
+      defaultActiveKey = defaultActiveKey || [];
     }
 
-    render() {
-        return <RcCollapse {...this.props} />;
+    return {
+      activeKey: activeKey || defaultActiveKey,
+    };
+  },
+
+  componentWillReceiveProps(nextProps) {
+    if ('activeKey' in nextProps) {
+      this.setState({
+        activeKey: nextProps.activeKey,
+      });
     }
-}
+  },
 
-Collapse.defaultProps = {
-    prefixCls: prefixCls
-};
+  handleClickItem(key) {
+    return () => {
+      const activeKey = this._getActivityKey();
+      if (this.props.accordion) {
+        this.setState({
+          activeKey: key === activeKey ? null : key,
+        });
+      } else {
+        const index = activeKey.indexOf(key);
+        const isActive = index > -1;
+        if (isActive) {
+          // remove active state
+          activeKey.splice(index, 1);
+        } else {
+          activeKey.push(key);
+        }
 
-Collapse.Panel = RcCollapse.Panel;
+        this.setState({activeKey: activeKey});
+      }
+      this.props.onChange(key);
+    };
+  },
 
-Collapse.displayName = "Collapse";
+  _getActivityKey() {
+    let activeKey = this.state.activeKey;
+    const { accordion } = this.props;
+    if (accordion && Array.isArray(activeKey)) {
+      activeKey = activeKey[0];
+    }
+
+    if (!accordion && !Array.isArray(activeKey)) {
+      activeKey = activeKey ? [activeKey] : [];
+    }
+    return activeKey;
+  },
+
+  getItems() {
+    const activeKey = this._getActivityKey();
+    const { prefixCls, accordion } = this.props;
+
+    return Children.map(this.props.children, (child, index) => {
+      // If there is no key provide, use the panel order as default key
+      const key = child.key || index;
+      const header = child.props.header;
+      let isActive = false;
+      if (accordion) {
+        isActive = activeKey === key;
+      } else {
+        isActive = activeKey.indexOf(key) > -1;
+      }
+
+      const props = {
+        key,
+        header,
+        isActive,
+        prefixCls,
+        children: child.props.children,
+        onItemClick: this.handleClickItem(key).bind(this),
+      };
+
+      return <CollapsePanel {...props} />;
+    });
+  },
+
+  render() {
+    const prefixCls = this.props.prefixCls;
+    return (
+      <div className={prefixCls}>
+        {this.getItems()}
+      </div>
+    );
+  },
+});
 
 export default Collapse;
---
+++ src/Panel.js
@@ -0,0 +1,57 @@
+import React, { PropTypes, createClass } from 'react';
+import classnames from 'classnames';
+
+const CollapsePanel = createClass({
+  propTypes: {
+    prefixCls: PropTypes.string,
+    header: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.node,
+    ]),
+    isActive: PropTypes.bool,
+    onItemClick: PropTypes.func,
+  },
+
+  getInitialState() {
+    return {isActive: this.props.isActive};
+  },
+
+  getDefaultProps() {
+    return {
+      isActive: false,
+      onItemClick() {
+      },
+    };
+  },
+
+  handleItemClick() {
+    this.props.onItemClick();
+  },
+
+  render() {
+    const { prefixCls, header, children, isActive } = this.props;
+    const headerCls = `${prefixCls}-header`;
+    const contentCls = classnames({
+      [`${prefixCls}-content`]: true,
+      [`${prefixCls}-content-active`]: isActive,
+    });
+
+    return (
+      <div className={`${prefixCls}-item`}>
+        <div className={headerCls} onClick={this.handleItemClick}
+             role="tab" aria-expanded={isActive}>
+          <i className="arrow"></i>
+          {header}
+        </div>
+          <div className={contentCls}
+               data-active={isActive}
+               role="tabpanel">
+            <div className={`${prefixCls}-content-box`}>{children}</div>
+          </div>
+      </div>
+    );
+  },
+});
+
+export default CollapsePanel;
